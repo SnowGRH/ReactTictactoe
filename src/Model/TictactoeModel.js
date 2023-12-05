f4e657dd880000944f556d9861d0b4eeefd83647
SnowGRH <gergelyovinczeff@gmail.com>
2023-12-05 14:34:02 +0100

    TicTacToe megy minden

---
+++ src/Model/TictactoeModel.js
@@ -0,0 +1,24 @@
+class TicTacToe{
+    #allapot;
+    #lepes;
+    #lista=[];
+    constructor(){
+        this.#allapot = " ";
+        this.#lista=[" "," ",""," ",""," "," "," ",""];
+        this.#lepes = 0;
+    }
+    setLista(index){
+        /* this.#lista[index] = "X" */
+        this.#lepes++;
+        if (this.#allapot === "X") {
+          this.#allapot = "O";
+        } else {
+          this.#allapot = "X";
+        }
+        this.#lista[index] = this.#allapot;
+    }
+    getLista(){
+        return[...this.#lista]
+    }
+}
+export default TicTacToe
